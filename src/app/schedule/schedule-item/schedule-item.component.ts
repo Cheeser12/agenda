b8d140769b61343f92b495022d0cb1238d16bbdb
Max Mays <max@maxmays.com>
2020-10-18 06:06:28 -0400

    Add ability to specify tasks per timestamp

--- src/app/schedule/schedule-item/schedule-item.component.ts
+++ src/app/schedule/schedule-item/schedule-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-schedule-item',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleItemComponent implements OnInit {
 
+  @Input()
+  timeStamp: string;
+  
   constructor() { }
 
   ngOnInit(): void {
